097babbe821d50637c85e39cc7b01413af80ad13
jeropardor <jeropardor3@gmail.com>
2024-03-07 18:14:56 +0100

    calendar

--- PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
+++ PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.panchagapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.panchagapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toolbar: Toolbar
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +37,23 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        // The Toolbar defined in the layout has the id "my_toolbar".
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.show()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_settings) {
+            Toast.makeText(this, "Clicked Settings Icon..", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
