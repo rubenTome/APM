f373784544cff7d806bb3f213f879fd9795b2160
Julian-BFacal <julian.bfacal@udc.es>
2024-03-07 10:20:41 +0100

    Cambiadas imagenes login e inscribir evento

---
+++ PachangAPP/app/src/main/java/com/example/panchagapp/ui/signin/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.panchagapp.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.panchagapp.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
