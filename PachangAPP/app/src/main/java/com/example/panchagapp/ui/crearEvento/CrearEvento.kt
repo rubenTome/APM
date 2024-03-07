91d8c80d3616feef521027213a087712b50dc211
Julian-BFacal <julian.bfacal@udc.es>
2024-03-07 20:40:45 +0100

    CrearEvento

---
+++ PachangAPP/app/src/main/java/com/example/panchagapp/ui/crearEvento/CrearEvento.kt
@@ -0,0 +1,12 @@
+package com.example.panchagapp.ui.crearEvento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.panchagapp.R
+
+class CrearEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_evento)
+    }
+}
