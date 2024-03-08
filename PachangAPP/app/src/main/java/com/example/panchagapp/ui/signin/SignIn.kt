e41c3d680daebd1d1d923871236973868ce1714f
Julian-BFacal <julian.bfacal@udc.es>
2024-03-08 17:37:41 +0100

    Boton Login Funcional

--- PachangAPP/app/src/main/java/com/example/panchagapp/ui/signin/SignIn.kt
+++ PachangAPP/app/src/main/java/com/example/panchagapp/ui/signin/SignIn.kt
@@ -1,13 +1,26 @@
 package com.example.panchagapp.ui.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.panchagapp.MainActivity
 import com.example.panchagapp.R
 
 class SignIn : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sig_in)
 
+        val loginbutton = findViewById<Button>(R.id.signInButton)
+         loginbutton.setOnClickListener {
+             val Intent = Intent(this,MainActivity::class.java)
+             startActivity(Intent)
+         }
+
     }
+
+
 }
