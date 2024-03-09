fcaa7e728607bee8558bc784e0680c1bdf55f9e7
Julian-BFacal <julian.bfacal@udc.es>
2024-03-09 13:19:28 +0100

    Arreglada toolbar

--- PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
+++ PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
@@ -6,11 +6,16 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.ToolbarWidgetWrapper
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.replace
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.panchagapp.databinding.ActivityMainBinding
@@ -18,10 +23,10 @@ import com.example.panchagapp.ui.home.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-class MainActivity : AppCompatActivity() {
+class   MainActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,30 +34,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val toolbar: Toolbar = binding.toolbar
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        setupActionBarWithNavController(navController)
         navView.setupWithNavController(navController)
-
-
-        // The Toolbar defined in the layout has the id "my_toolbar".
-        //setSupportActionBar(findViewById(R.id.my_toolbar))
-        //supportActionBar?.show()
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
     }
 
-/*
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.top_bar_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.nav_settings) {
-            Toast.makeText(this, "Clicked Settings Icon..", Toast.LENGTH_SHORT).show()
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+
+        /*
+        return when(item.itemId) {
+            R.id.appbar_menu -> {
+                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.appbar_profile -> {
+                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.appbar_coins -> {
+                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
+                return true
+            } else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
+        */
+
+
     }
-*/
 }
