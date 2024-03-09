5ec5040b98d58128fefd02c13b7cc5fc456f7b9b
Julian-BFacal <julian.bfacal@udc.es>
2024-03-09 22:30:28 +0100

    Fix varios botons

--- PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
+++ PachangAPP/app/src/main/java/com/example/panchagapp/MainActivity.kt
@@ -1,32 +1,43 @@
 package com.example.panchagapp
 
 import android.os.Bundle
+import android.text.Layout.Directions
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.replace
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.panchagapp.databinding.ActivityMainBinding
 import com.example.panchagapp.ui.home.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationView
 
 
 class   MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,16 +46,31 @@ class   MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
         val toolbar: Toolbar = binding.toolbar
+        val sidemenu : NavigationView = binding.sideView
+        val drawerlayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_settings, R.id.nav_about,R.id.nav_share),
+            drawerlayout
+        )
+
+        toggle =  ActionBarDrawerToggle(this,drawerlayout, toolbar, R.string.open,R.string.close)
+        drawerlayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
         navView.setupWithNavController(navController)
+        sidemenu.setupWithNavController(navController)
+
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,25 +78,15 @@ class   MainActivity : AppCompatActivity() {
         return true
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-
-        /*
-        return when(item.itemId) {
-            R.id.appbar_menu -> {
-                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.appbar_profile -> {
-                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.appbar_coins -> {
-                Toast.makeText(this,"HOLA",Toast.LENGTH_SHORT).show()
-                return true
-            } else -> super.onOptionsItemSelected(item)
+         if (item.itemId == R.id.appbar_profile) {
+            val action = MobileNavigationDirections.actionGlobalNavigationProfile()
+            navController.navigate(action)
+            return true
+        } else {
+            return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
         }
-        */
-
-
     }
+
+
+
 }
